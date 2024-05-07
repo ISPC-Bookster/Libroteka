d2bf3cddc79526de5e24f6bd648852933180e46b
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2024-05-07 00:51:26 -0300

    agregue las rutas a pagina sobrenosotros,home y contacto

--- Frontend/src/app/components/sobre-nosotros/sobre-nosotros.component.ts
+++ Frontend/src/app/components/sobre-nosotros/sobre-nosotros.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sobre-nosotros',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sobre-nosotros.component.html',
   styleUrl: './sobre-nosotros.component.css'
 })
