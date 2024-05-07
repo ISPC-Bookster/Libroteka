043740781a9bec1d828edff5f6acd48b4c86544e
Florencia Maldonado <flor_maldonado13@hotmail.com>
2024-05-06 21:28:51 -0300

    primer commit

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SobreNosotrosComponent } from "./components/sobre-nosotros/sobre-nosotros.component"
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SobreNosotrosComponent, ContactoComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
