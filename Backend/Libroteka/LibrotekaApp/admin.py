616a34dde9873f97ec253833eac8aded981b9007
JuanSuarez <jp.juansuarez@gmail.com>
2024-05-07 23:10:22 -0300

    Revert "Esto es lo que hice"

--- Backend/Libroteka/LibrotekaApp/admin.py
+++
@@ -1,7 +0,0 @@
-
-
-from django.contrib import admin
-from .models import Product, CarritoItem  # Corrige la importación
-
-admin.site.register(Product)
-admin.site.register(CarritoItem) 
--- Backend/Libroteka/LibrotekaApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibrotekaappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LibrotekaApp'
