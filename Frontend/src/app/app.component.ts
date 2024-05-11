2de38827bf9e5b8b8a58842631daccef6f297ffb
Florencia Maldonado <flor_maldonado13@hotmail.com>
2024-05-10 23:04:58 -0300

    Creación de contacto y footer

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SobreNosotrosComponent } from "./components/sobre-nosotros/sobre-nosotros.component"
+import { SobreNosotrosComponent } from "./components/sobre-nosotros/sobre-nosotros.component";
 import { ContactoComponent } from './components/contacto/contacto.component';
-
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SobreNosotrosComponent, ContactoComponent ],
+  imports: [RouterOutlet, SobreNosotrosComponent, ContactoComponent, FooterComponent, HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
