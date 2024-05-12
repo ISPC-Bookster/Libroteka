48a13e3a8982af4e3ab6a07aaa8aaaf6920106c4
JuanPSuarez <jp.juansuarez@gmail.com>
2024-05-12 20:20:41 -0300

    Fixed conflicts, added sobre-nosotro routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CreateComponent } from './components/create/create.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
 
 export const routes: Routes = [
     {path: 'catalogo', component: CatalogoComponent },
     {path: 'home', component: LandingComponent},
     {path: 'contacto', component: ContactoComponent},
     {path: 'socialnet', component: SocialnetComponent },
+    {path: 'sobre-nosotros', component: SobreNosotrosComponent},
     {path: 'create', component: CreateComponent },
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', redirectTo: '/home', pathMatch: 'full'} //reemplazar por componente: 404, page not fount libroteka
