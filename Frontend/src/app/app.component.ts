f3c2bd3ca0ceadb83591c186cc71d817dff78b3a
JuanPSuarez <jp.juansuarez@gmail.com>
2024-05-11 21:16:20 -0300

    fixed, and added routes. Fixed styles in shared components

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
-import { LoginComponent } from './Login/login/login.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, LandingComponent, NavbarComponent, FooterComponent, HeaderComponent, ContactoComponent, SobreNosotrosComponent, LoginComponent
+    RouterOutlet, LandingComponent, NavbarComponent, FooterComponent, HeaderComponent, ContactoComponent, SobreNosotrosComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: LandingComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', redirectTo: '/home', pathMatch: 'full'} //reemplazar por componente: 404, page not fount libroteka
+
+];
