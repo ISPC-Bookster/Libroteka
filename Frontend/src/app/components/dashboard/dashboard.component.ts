4e1df6a53fd4df78594482c3978f05b013517cd0
JuanPSuarez <jp.juansuarez@gmail.com>
2024-05-13 01:12:33 -0300

    Added and created dashboard component.html

--- Frontend/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -8,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  recentPurchases = [
+    {
+      orderId: 1234,
+      books: [
+        { title: 'El Señor de los Anillos', price: 25000 },
+        { title: 'Cien Años de Soledad', price: 20000 }
+      ],
+      total: 45
+    },
+    {
+      orderId: 5678,
+      books: [
+        { title: 'Sapiens', price: 30 },
+        { title: 'The Alchemist', price: 15 }
+      ],
+      total: 45
+    }
+  ];
+
+  shipmentStatus = [
+    {
+      orderId: 1234,
+      status: 'In Transit',
+      estimatedDelivery: 'May 16, 2024',
+      trackingLink: 'https://www.waredock.com/magazine/complete-guide-to-tracking/',
+      carrier: 'Courier Service'
+    },
+    {
+      orderId: 5678,
+      status: 'Delivered',
+      deliveryDate: 'May 10, 2024',
+      trackingLink: 'https://www.waredock.com/magazine/complete-guide-to-tracking/',
+      carrier: 'National Post'
+    }
+  ];
 
 }
