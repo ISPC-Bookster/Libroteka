613f60faec8ce75a6c64aae6b02bd471a8cf1f16
pupaycleo <segalldaniel60@gmail.com>
2024-05-12 21:22:52 -0300

    agregue rutas a mis componentes

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
+import { LoginComponent } from './components/login/login.component';
+import { BusquedaPersonalizadaComponent } from './components/busqueda-personalizada/busqueda-personalizada.component';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +23,8 @@ import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotr
     HeaderComponent, 
     ContactoComponent, 
     SobreNosotrosComponent,
+    LoginComponent,
+    BusquedaPersonalizadaComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Login/login/login.component';
 import { BusquedaPersonalizadaComponent } from './components/busqueda-personalizada/busqueda-personalizada.component';
 import { SocialnetComponent } from './components/socialnet/socialnet.component';
 import { CreateComponent } from './components/create/create.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    {path: 'login', component: LoginComponent },
+    {path: 'busqueda-personalizada', component: BusquedaPersonalizadaComponent },
     {path: 'catalogo', component: CatalogoComponent },
     {path: 'home', component: LandingComponent},
     {path: 'contacto', component: ContactoComponent},
@@ -15,7 +17,6 @@ export const routes: Routes = [
     {path: 'create', component: CreateComponent },
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', redirectTo: '/home', pathMatch: 'full'},//reemplazar por componente: 404, page not fount libroteka
-    {path: 'login', component: LoginComponent },
-    {path: 'busqueda-personalizada', component: BusquedaPersonalizadaComponent },
+    
 ];
 
