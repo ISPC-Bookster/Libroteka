dc2f62a6c0265dfa4e03b4728d5aebcc6a246d4c
giuliana <giuliana.gesto@bitlogic.io>
2024-05-12 21:02:17 -0300

    wip: models

--- Backend/Libroteka/Libroteka/settings.py
+++ Backend/Libroteka/Libroteka/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'librotekasite.apps.LibrotekasiteConfig'
 ]
 
 MIDDLEWARE = [
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'librotekasite.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
