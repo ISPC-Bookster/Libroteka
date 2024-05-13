1e592b38c545879c5a45eaab94ba075de1f35acf
JuanPSuarez <jp.juansuarez@gmail.com>
2024-05-12 21:09:02 -0300

    Fixed routes, login, busqueda-personalizada

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Login/login/login.component';
 import { BusquedaPersonalizadaComponent } from './components/busqueda-personalizada/busqueda-personalizada.component';
 import { SocialnetComponent } from './components/socialnet/socialnet.component';
 import { CreateComponent } from './components/create/create.component';
@@ -7,6 +6,7 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: 'catalogo', component: CatalogoComponent },
@@ -15,9 +15,9 @@ export const routes: Routes = [
     {path: 'socialnet', component: SocialnetComponent },
     {path: 'sobre-nosotros', component: SobreNosotrosComponent},
     {path: 'create', component: CreateComponent },
+    {path: 'login', component:  LoginComponent},
+    {path: 'busqueda-personalizada', component: BusquedaPersonalizadaComponent },
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', redirectTo: '/home', pathMatch: 'full'},//reemplazar por componente: 404, page not fount libroteka
-    {path: 'login', component: LoginComponent },
-    {path: 'busqueda-personalizada', component: BusquedaPersonalizadaComponent },
 ];
 
