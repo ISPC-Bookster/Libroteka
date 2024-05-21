13350a9debe119be2852c4361aacc847e7477ea5
JuanPSuarez <jp.juansuarez@gmail.com>
2024-05-20 23:35:31 -0300

    Added some basic structure to book component

--- Frontend/src/app/components/book-details/book-details.component.ts
+++ Frontend/src/app/components/book-details/book-details.component.ts
@@ -20,5 +20,11 @@ interface Book {
   styleUrl: './book-details.component.css'
 })
 export class BookDetailsComponent {
-
+  title= "titulo 1"
+  id_Author= "Un Autor"
+  id_Genre= "Terror"
+  description= "Esto es una descripcion"
+  price= 4000;
+  stock= 10;
+  id_Editorial= "Anagrama";
 }
