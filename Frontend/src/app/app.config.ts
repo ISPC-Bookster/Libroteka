24ec3f2cb95e1dd048d3fffb15e78aaf4340a4fd
pupaycleo <segalldaniel60@gmail.com>
2024-06-01 17:16:34 -0300

    integrate search

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, HttpClientModule } from '@angular/common/http';
